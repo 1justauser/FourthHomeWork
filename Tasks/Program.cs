362cdfa927eb7dd31669aa9ad6a35d7d8e26d78b
Nursultan <superrandommmm@gmail.com>
2024-11-21 08:33:06 +0300

    Add project files.

---
+++ Tasks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Tasks
+{
+    public class Project()
+    {
+        static void Main(string[] args) 
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+
+            void StartTasks(int n)
+            {
+                Console.WriteLine("Задание {0}", n);
+            }
+
+
+            StartTasks(0);
+            Console.WriteLine("Русский язык работает");
+        }
+    }
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Tumakov
+{
+    public class Project()
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+
+            Console.WriteLine("Русский язык работает");
+        }
+    }
+}
